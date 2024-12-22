113c75cc9ed349c441d55a4833e077f5e426841b
glsh0725 <lsh920725@naver.com>
2024-12-22 22:50:59 +0900

    init

---
+++ src/test/java/fs/four/eatery/EateryPrjtApplicationTests.java
@@ -0,0 +1,13 @@
+package fs.four.eatery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EateryPrjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
