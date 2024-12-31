85a1ead55890fa5acc5898df559766eeaaa9489a
HyunSeopPark <qkrgustjq123@naver.com>
2024-12-31 09:23:28 +0900

    자꾸 왜 지맘대로 쳐바꾸고 지랄
    깃이 뭔가 이상한거 같아요 시후형 이거 푸시해도 자꾸 지맘대로 바뀌고 파일명 대문자로했는데 소문자로 바뀌고 막그래요

--- src/main/frontend/src/pages/login.js
+++ src/main/frontend/src/pages/login.js
@@ -3,39 +3,29 @@ import DiningLayout from "../layouts/DiningLayout";
 import "../css/Login.css";
 
 const Login = () => {
-  return (
-      <DiningLayout>
-        <form className="login-form">
-          <h1>로그인</h1>
-          <input className="input-field" type="text" id="id" placeholder="아이디" />
-          <input className="input-field" type="text" id="password" placeholder="비밀번호"/>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-          <div className="label">
-            <input type="checkbox" id="confirm-password"/> 아이디 저장
-=======
-          <div>
-            <input className="label" type="checkbox" id="confirm-password"/> 아이디 저장
->>>>>>> Stashed changes
-=======
-          <div>
-            <input className="label" type="checkbox" id="confirm-password"/> 아이디 저장
->>>>>>> Stashed changes
-          </div>
-          <button className="login-button">로그인</button>
-          <div className="label">회원가입</div>
-          <div className="label">아이디/비밀번호찾기</div>
-          <div className="display">
-            <button type="submit" className="signup-button">
-              카카오 아이디로 로그인
-            </button>
-            <button type="submit" className="signup-button">
-              구글계정으로 로그인
-            </button>
-          </div>
-        </form>
-      </DiningLayout>
-  );
+    return (
+        <DiningLayout>
+            <form className="login-form">
+                <h1>로그인</h1>
+                <input className="input-field" type="text" id="id" placeholder="아이디" />
+                <input className="input-field" type="text" id="password" placeholder="비밀번호"/>
+                <div className="label">
+                    <input type="checkbox" id="confirm-password"/> 아이디 저장
+                </div>
+                <button className="login-button">로그인</button>
+                <div className="label">회원가입</div>
+                <div className="label">아이디/비밀번호찾기</div>
+                <div className="display">
+                    <button type="submit" className="signup-button">
+                        카카오 아이디로 로그인
+                    </button>
+                    <button type="submit" className="signup-button">
+                        구글계정으로 로그인
+                    </button>
+                </div>
+            </form>
+        </DiningLayout>
+    );
 };
 
 export default Login;
